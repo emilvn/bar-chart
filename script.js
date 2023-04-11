8d4fe4ae021f701984c31bdf7b9c65bd4103ef15
emilvn <emilvnielsen@hotmail.com>
2023-04-11 22:22:51 +0200

    added comments

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 "use strict";
 window.addEventListener("load", main);
-
+/* ========== updates chart every 1000 ms ========== */
 function main() {
     const customers = getQueueData();
     setInterval(() => {
         updateBarChart(customers);
     }, 1000);
 }
-
+/* ========== calls all necessary funcs to update chart ========== */
 function updateBarChart(customers) {
     removeAnimation(customers);
     addAnimation(customers);
     addQueueData(customers);
-    displayInfoWindow(customers);
+    handleBarHover(customers);
 }
-
+/* ========== adds the queue data to array and sets bar heights ========== */
 function addQueueData(customers) {
     const queueSize = getNumberOfCustomers();
     customers.push(queueSize);
     customers.shift();
     setBarHeight(customers);
 }
-
+/* ========== functions for creating array from random numbers ========== */
 function getQueueData() {
     const customers = [];
     for (let i = 0; i < 40; i++) {
@@ -29,29 +29,27 @@ function getQueueData() {
     }
     return customers;
 }
-
 function getNumberOfCustomers() {
     return Math.floor(Math.random() * 32);
 }
-
+/* ========== sets bar height and changes the bar text value to it ========== */
 function setBarHeight(customers) {
     for (let i = 0; i < customers.length; i++){
-        document.querySelectorAll("#barChart div")[i].style.height = `${customers[i]}vh`;
+        const bar = document.querySelectorAll("#barChart div")[i];
+        bar.style.height = `${customers[i]}vh`;
+        bar.querySelector("span").textContent = customers[i];
     }
 }
-
-function displayInfoWindow(customers) {
-    const infoWindow = document.querySelector("#info-window")
+/* ========== event listeners for showing bar value on mouse hover ========== */
+function handleBarHover(customers) {
     for (let i = 0; i < customers.length; i++){
-        document.querySelectorAll("#barChart div")[i].addEventListener("mouseover", () => {
-            infoWindow.textContent = customers[i];
-            infoWindow.style.display = "block";
-        })
-        document.querySelectorAll("#barChart div")[i].addEventListener("mouseout", () => {
-            infoWindow.style.display = "none";
-        })
+        const bar = document.querySelectorAll("#barChart div")[i];
+        const span = bar.querySelector("span");
+        bar.addEventListener("mouseover", () => span.style.display = "inline");
+        bar.addEventListener("mouseleave", () => span.style.display = "none");
     }
 }
+/* ========== functions for smooth sliding animation of chart ========== */
 function addAnimation(customers) {
     for (let i = 0; i < customers.length; i++){
         document.querySelectorAll("#barChart div")[i].classList.add("slide");
