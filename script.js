6611dd1ee019987f7094b9fa59e59a1e1babac31
emilvn <emilvnielsen@hotmail.com>
2023-04-11 14:24:45 +0200

    works now

---
+++ script.js
@@ -0,0 +1,25 @@
+"use strict";
+window.addEventListener("load", main);
+
+function main() {
+    const customers = [];
+    for (let i = 0; i < 40; i++){
+        customers[i] = getNumberOfCustomers();
+    }
+    setInterval(() => {
+        const queueSize = getNumberOfCustomers();
+        customers.push(queueSize);
+        customers.shift();
+        setBarHeight(customers);
+    }, 1000);
+}
+
+function getNumberOfCustomers() {
+    return Math.floor(Math.random() * 32);
+}
+
+function setBarHeight(customers) {
+    for (let i = 0; i < customers.length; i++){
+        document.querySelector(`#bar${i}`).style.height = `${customers[i]*2}px`;
+    }
+}
